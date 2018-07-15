71a5960f065279283a50f7565b15eef21b9dd96c
edus44 <eduhhg@gmail.com>
2018-07-15 12:08:39 +0200

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = (api, projectOptions) => {
+  api.chainWebpack(webpackConfig => {
+    config.resolve.extensions.add('.yml').add('.yaml')
+    config.module
+      .rule('yaml')
+        .test(/\.ya?ml?$/)
+        .use('json-loader')
+          .loader('json-loader')
+        .end()
+        .use('yaml-loader')
+          .loader('yaml-loader')
+        .end()
+  })
+}
